c80a651743c6bbda26c7c2308cff3daa35ab10d4
redraincatching <redraincatching@disroot.org>
2023-09-12 15:10:31 +0100

    edited using clamp trait and formatting flags to be more concise

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,10 @@
 pub fn rgb(r: i32, g: i32, b: i32) -> String {
-    // check each to see if single char, at which point add to format
-    format!("{}{}{}", to_hex(r), to_hex(g), to_hex(b))
-}
+    // format as 2-char with leading zero if necessary
+    // then use clamp to keep in range 0-255
+    format!("{:02X}{:02X}{:02X}", 
+        r.clamp(0,255),
+        g.clamp(0,255),
+        b.clamp(0,255)
+    )
 
-fn to_hex(num: i32) -> String {
-    if num < 0 { return String::from("00") }
-    if num > 255 { return String::from("FF") }
-    let conv: String = format!("{:X}", num);
-    if conv.len() == 1 {
-        return "0".to_owned() + &conv
-    }
-    println!("input: {}\nconverted: {:X}", num, num);
-    return conv
 }
